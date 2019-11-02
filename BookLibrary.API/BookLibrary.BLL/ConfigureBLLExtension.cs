03011372f8e778141bbc9608fa1ffe1560cbb67e
Vitalii Maksymiv <pbbbbc@gmail.com>
2019-11-02 22:55:05 +0200

    Configure BLL

--- BookLibrary.API/BookLibrary.BLL/ConfigureBLLExtension.cs
+++ BookLibrary.API/BookLibrary.BLL/ConfigureBLLExtension.cs
@@ -1,4 +1,7 @@
-﻿using BookLibrary.DAL;
+﻿using BookLibrary.BLL.Factory;
+using BookLibrary.BLL.Services.Implemented;
+using BookLibrary.BLL.Services.Interfaces;
+using BookLibrary.DAL;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,6 +12,15 @@ namespace BookLibrary.BLL
         public static void ConfigureBLL(this IServiceCollection services, IConfiguration configuration)
         {
             services.ConfigureDAL(configuration);
+            services.ConfigureServices();
+
+            services.AddScoped<IServiceFactory, ServiceFactory>();
+        }
+
+        private static void ConfigureServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthorService, AuthorService>();
+            services.AddScoped<IBookService, BookService>();
         }
     }
 }
